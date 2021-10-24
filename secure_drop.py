b8247d4fc99fb03b11d36936d43dd1196d7ccac8
Joseph Gosselin <josephsgosselin@outlook.com>
2021-10-24 11:11:33 -0400

    Added support for user password hashing and login. able to display and run shell. Able to add unencrypted contact information and store it in file. Program expected to only work with a single user, but i believe that is within the scope with the project.

--- secure_drop.py
+++ secure_drop.py
@@ -2,6 +2,11 @@ import json
 import getpass
 from Crypto.Cipher import AES
 
+from base64 import b64encode
+from Crypto.Hash import SHA256
+from Crypto.Protocol.KDF import bcrypt, bcrypt_check
+
+
 def main():
     myFile = open("information.json")
     jsonFile = json.load(myFile)
@@ -29,13 +34,27 @@ def registerUser():
                 print ("Exiting User Registration\n")
                 runLoop = False
 
+def generateHash(passwordString):
+    password = str(passwordString).encode()
+    b64pwd = b64encode(SHA256.new(password).digest())
+    return bcrypt(b64pwd,12)
+
+def verifyPassword(password, hash1):
+    try:
+        b64pwd = b64encode(SHA256.new(str(password).encode()).digest())
+        inBytes = str(hash1).encode()
+        bcrypt_check(b64pwd,str(hash1).encode())
+        return True
+    except:
+        print()
+ 
 def addUser():
     myFile = open("information.json",'r+')
     jsonLoaded = json.load(myFile)
 
     fullName = input ("\nEnter Full Name: ")
     emailData = input ("Enter Email Address: ")
-    passwordData = getpass.getpass ("Enter Password: ")
+    passwordData =  getpass.getpass ("Enter Password: ")
     passwordAgain = getpass.getpass ("Re-enter Password: ")
 
     if passwordData != passwordAgain:
@@ -43,7 +62,7 @@ def addUser():
         print ("Exiting SecureDrop\n")
 
     else:
-        data = {"name":fullName,"email": emailData, "password": passwordData}
+        data = {"name":fullName,"email": emailData, "password": generateHash(passwordData).decode(),"Contacts":[] }
         jsonLoaded["user"].append(data)
         myFile.seek(0)
         json.dump(jsonLoaded,myFile, indent=4)
@@ -51,9 +70,74 @@ def addUser():
         print ("\nPasswords Match.")
         print ("User Registered.")
         print ("Exiting SecureDrop.\n")
+def addContact(password):
+    myFile = open("information.json",'r+')
+    jsonLoaded = json.load(myFile)
+    contactName = input("Enter Full Name: ")
+    contactEmail = input ("Enter Email Address: ")
+
+
+    data = {"name":contactName,"email":contactEmail}
+    jsonLoaded["user"][0]["Contacts"].append(data)
+    myFile.seek(0)
+    json.dump(jsonLoaded,myFile, indent=4)
+    myFile.close()
+
+def verifyUser(email,password):
+    myFile = open("information.json",'r')
+    jsonLoaded = json.load(myFile)
+    for user in jsonLoaded["user"]:
+        if user["email"] == email:
+            if verifyPassword(password, user["password"]):
+                return True
+    print ("Incorrect Password/Email Combination\n")
+    return False     
+
+def listCommands():
+    print ("\"add\"  -> Add a new contact")
+    print ("\"list\" -> List all online contacts")
+    print ("\"send\" -> Transfer file to contacts")
+    print ("\"exit\" -> Exit SecureDrop")
+
+def runShell(password):
+    shouldRun = True
+    print ("Welcome to SecureDrop")
+    print("Type \"help\" For Commands.")
+    print()
+    while (shouldRun):
+        userResponse = input("secure_drop> ")
+        userResponse = userResponse.lower()
+        if(userResponse == "help"):
+            listCommands()
+        elif(userResponse == "add"):
+            addContact(password)
+        elif (userResponse == "send"):
+            print("sending")
+        elif(userResponse == "list"):
+            print("listing")
+        elif(userResponse == "exit"):
+            exit()
+        else:
+            print("Unknown Command")
+
+    
+
+
 
 def login():
-    runLoop = True
+    shouldLogin = False
+    trys = 0
+    while trys < 3 and shouldLogin != True:
+        email = input("Enter Email Address: ")
+        password = getpass.getpass("Enter Password: ")
+        shouldLogin = verifyUser(email, password)
+        trys = trys + 1
+    if (shouldLogin):
+        runShell(password)
+    else:
+        print ("To many attempts to login")
+        print()
+        exit()
 
 
 if __name__ == "__main__":
