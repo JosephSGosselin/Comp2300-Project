7a4407c06b1929cfafa72e740a8ceb13ed091092
Joseph Gosselin <josephsgosselin@outlook.com>
2021-11-29 23:02:56 -0500

    Created User object class and moved some classes around changed how contacts are stored, made it that they have their own seperate json file Added encryption and decryption of contacts with validation infromation.json and contacts.json will be generated if not present. added comments to code
    TODO: find a cleaner way to store functions?

--- secure_drop.py
+++ secure_drop.py
@@ -1,128 +1,29 @@
 import json
-import getpass
-from Crypto.Cipher import AES
-
-from base64 import b64encode
-from Crypto.Hash import SHA256
-from Crypto.Protocol.KDF import bcrypt, bcrypt_check
-
+from utils import *
 
 def main():
-    myFile = open("information.json")
+
+    #initalizes information.json if not created.
+    try:  
+        myFile = open("information.json")
+    except:
+        myFile = open("information.json","w+")
+        myFile.write("{ \t\"user\": [] }")
+        myFile.seek(0)
+        print("Information.json was created...")
+
     jsonFile = json.load(myFile)
     myFile.close()
-
     registered =  (len(jsonFile["user"]) >= 1)
 
     if not registered:
         print ("No users are registered with this client.")
+        #in this function, user will be registered, also checks for contacts.json was created
+        #this is required to check for contacts since this is the first func that comes
+        #in contact with the password to used to encrypt.
         registerUser()
     else:
         login()
-
-def registerUser():
-    runLoop = True
-    while runLoop:
-        userResponse = input("Would you like to register a user (y/n)? ")
-        if (userResponse.lower() != "y") and (userResponse.lower() != "n"):
-            print ("invalid response...\n")
-        else:
-            if userResponse.lower() == 'y':
-                addUser()
-                runLoop = False
-            else:
-                print ("Exiting User Registration\n")
-                runLoop = False
-
-def generateHash(passwordString):
-    password = str(passwordString).encode()
-    b64pwd = b64encode(SHA256.new(password).digest())
-    return bcrypt(b64pwd,12)
-
-def verifyPassword(password, hash1):
-    try:
-        b64pwd = b64encode(SHA256.new(str(password).encode()).digest())
-        inBytes = str(hash1).encode()
-        bcrypt_check(b64pwd,str(hash1).encode())
-        return True
-    except:
-        print()
- 
-def addUser():
-    myFile = open("information.json",'r+')
-    jsonLoaded = json.load(myFile)
-
-    fullName = input ("\nEnter Full Name: ")
-    emailData = input ("Enter Email Address: ")
-    passwordData =  getpass.getpass ("Enter Password: ")
-    passwordAgain = getpass.getpass ("Re-enter Password: ")
-
-    if passwordData != passwordAgain:
-        print ("\nPasswords do not match.")
-        print ("Exiting SecureDrop\n")
-
-    else:
-        data = {"name":fullName,"email": emailData, "password": generateHash(passwordData).decode() test,"Contacts":[] }
-        jsonLoaded["user"].append(data)
-        myFile.seek(0)
-        json.dump(jsonLoaded,myFile, indent=4)
-        myFile.close()
-        print ("\nPasswords Match.")
-        print ("User Registered.")
-        print ("Exiting SecureDrop.\n")
-def addContact(password):
-    myFile = open("information.json",'r+')
-    jsonLoaded = json.load(myFile)
-    contactName = input("Enter Full Name: ")
-    contactEmail = input ("Enter Email Address: ")
-
-
-    data = {"name":contactName,"email":contactEmail}
-    jsonLoaded["user"][0]["Contacts"].append(data)
-    myFile.seek(0)
-    json.dump(jsonLoaded,myFile, indent=4)
-    myFile.close()
-
-def verifyUser(email,password):
-    myFile = open("information.json",'r')
-    jsonLoaded = json.load(myFile)
-    for user in jsonLoaded["user"]:
-        if user["email"] == email:
-            if verifyPassword(password, user["password"]):
-                return True
-    print ("Incorrect Password/Email Combination\n")
-    return False     
-
-def listCommands():
-    print ("\"add\"  -> Add a new contact")
-    print ("\"list\" -> List all online contacts")
-    print ("\"send\" -> Transfer file to contacts")
-    print ("\"exit\" -> Exit SecureDrop")
-
-def runShell(password):
-    shouldRun = True
-    print ("Welcome to SecureDrop")
-    print("Type \"help\" For Commands.")
-    print()
-    while (shouldRun):
-        userResponse = input("secure_drop> ")
-        userResponse = userResponse.lower()
-        if(userResponse == "help"):
-            listCommands()
-        elif(userResponse == "add"):
-            addContact(password)
-        elif (userResponse == "send"):
-            print("sending")
-        elif(userResponse == "list"):
-            print("listing")
-        elif(userResponse == "exit"):
-            exit()
-        else:
-            print("Unknown Command")
-
-    
-
-
 
 def login():
     shouldLogin = False
@@ -138,6 +39,42 @@ def login():
         print ("To many attempts to login")
         print()
         exit()
+
+def runShell(password):
+
+    #when user account is made, it is taking the info from json file.
+    user = User()
+
+    shouldRun = True
+    encryptionKey = generateEncrpytionHash(password, user.salt)
+    print ("Welcome to SecureDrop")
+    print("Type \"help\" For Commands.")
+    print()
+    while (shouldRun):
+        userResponse = input("secure_drop> ")
+        userResponse = userResponse.lower()
+        if(userResponse == "help"):
+            listCommands()
+        elif(userResponse == "add"):
+            decryptContacts(encryptionKey)
+            addContact()
+            encryptContacts(encryptionKey)
+        elif (userResponse == "send"):
+            print("sending")
+        elif(userResponse == "list"):
+            print("listing")
+        elif(userResponse == "exit"):
+            del password
+            del encryptionKey
+            exit()
+        else:
+            print("Unknown Command")
+
+def listCommands():
+    print ("\"add\"  -> Add a new contact")
+    print ("\"list\" -> List all online contacts")
+    print ("\"send\" -> Transfer file to contacts")
+    print ("\"exit\" -> Exit SecureDrop")
 
 
 if __name__ == "__main__":
---
+++ utils.py
@@ -0,0 +1,181 @@
+import json
+import getpass
+import string
+import random
+from base64 import b64encode
+from Crypto.Hash import SHA256
+from Crypto.Cipher import AES
+from Crypto.Protocol.KDF import bcrypt, bcrypt_check, PBKDF2
+from User import *
+
+#used for password verification
+def generateHash(passwordString):
+    password = str(passwordString).encode()
+    b64pwd = b64encode(SHA256.new(password).digest())
+    return bcrypt(b64pwd,12)
+
+#used to add first user, not the contacts thats different
+def addUser():
+    myFile = open("information.json",'r+')
+    jsonLoaded = json.load(myFile)
+
+    fullName = input ("\nEnter Full Name: ")
+    emailData = input ("Enter Email Address: ")
+    passwordData =  getpass.getpass ("Enter Password: ")
+    passwordAgain = getpass.getpass ("Re-enter Password: ")
+
+    if passwordData != passwordAgain:
+        print ("\nPasswords do not match.")
+        print ("Exiting SecureDrop\n")
+
+    else:
+        generatedSalt = get_random_string(32)
+        data = {"name":fullName,"email": emailData, "password": generateHash(passwordData).decode(),"salt": generatedSalt }
+        jsonLoaded["user"].append(data)
+        myFile.seek(0)
+        json.dump(jsonLoaded,myFile, indent=4)
+        myFile.close()
+
+        encryptionKey = generateEncrpytionHash(passwordData, generatedSalt)
+
+        #if file exist, great move on
+        try:  
+            myFile = open("contacts.json")
+        except:
+            #if not, create the template and finish
+            myFile = open("contacts.json","w+")
+            myFile.write("{ \t\"contacts\": [] }")
+            myFile.seek(0)
+            print("\ncontacts.json was created and encrypted...")
+
+        encryptContacts(encryptionKey)
+        myFile.close()
+        del encryptionKey
+        del passwordData
+        del passwordAgain
+
+
+        print ("\nPasswords Match.")
+        print ("User Registered.")
+        print ("Exiting SecureDrop.\n")
+
+#this is used to generate an ascii salt of any length
+def get_random_string(length):
+    letters = string.ascii_lowercase + string.ascii_uppercase + "0123456789"
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+#used to login
+def verifyUser(email,password):
+    myFile = open("information.json",'r')
+    jsonLoaded = json.load(myFile)
+    for user in jsonLoaded["user"]:
+        if user["email"] == email:
+            if verifyPassword(password, user["password"]):
+                return True
+    print ("Incorrect Password/Email Combination\n")
+    return False
+
+#when login, check the stored hashed password with the attempted sign in password
+def verifyPassword(password, hash1):
+    try:
+        b64pwd = b64encode(SHA256.new(str(password).encode()).digest())
+        bcrypt_check(b64pwd,str(hash1).encode())
+        return True
+    except:
+        print()
+
+#used to shorten/make longer someones password, and hashed to 256 bits
+#this will be used to encrpyted using AES
+def generateEncrpytionHash (password, storedSalt):
+    salt = str(storedSalt).encode()
+    key = PBKDF2(password,salt,32)
+    return key
+
+def addContact():
+    myFile = open("contacts.json",'r+')
+    jsonLoaded = json.load(myFile)
+    contactName = input("Enter Full Name: ")
+    contactEmail = input ("Enter Email Address: ")
+    data = {"name":contactName,"email":contactEmail}
+    Contacts = jsonLoaded["contacts"]
+
+    #updates contacts if needed
+    if Contacts == []:
+        Contacts.append(data)
+        print ("Contact added.\n")
+    elif contactEmail in [x["email"] for x in Contacts]:
+        count = 0
+        for x in range(0,len(Contacts)):
+            if Contacts[x]["email"] == contactEmail:
+                count = x
+        Contacts[x]["name"] = contactName
+        print ("Contact updated.\n")
+    else:
+        Contacts.append(data)
+        print ("Contact added.\n")
+
+    myFile.seek(0)
+    json.dump(jsonLoaded,myFile, indent=4)
+    myFile.close()
+
+def registerUser():
+    runLoop = True
+    while runLoop:
+        userResponse = input("Would you like to register a user (y/n)? ")
+        if (userResponse.lower() != "y") and (userResponse.lower() != "n"):
+            print ("invalid response...\n")
+        else:
+            if userResponse.lower() == 'y':
+                addUser()
+                runLoop = False
+            else:
+                print ("Exiting User Registration\n")
+                runLoop = False
+
+def encryptContacts (key):
+    #gets data, clears file, closes.
+    myFile = open("contacts.json","r+")
+    data = ""
+    for i in myFile:
+        data+=i
+
+    #clears file aso it gets ready for the encrypted data
+    myFile.truncate(0)
+    myFile.close()
+
+    cipher = AES.new(key, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(data.encode())
+
+    #reopens file with correct mode for encryption
+    file_out = open("contacts.json","wb")
+    [ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
+    file_out.close()
+
+def decryptContacts (key):
+    #opens the encrypted contacts
+    #reads in 3 items to verify and decrypt
+    myFile = open("contacts.json","rb")
+    nonce, tag, ciphertext = [ myFile.read(x) for x in (16, 16, -1) ]
+    myFile.close()
+
+    cipher = AES.new(key, AES.MODE_EAX, nonce)
+
+    #if can decrypt suceffully then do so
+    try:
+
+        data = cipher.decrypt_and_verify(ciphertext, tag)
+    except:
+        #deletes contacts if validity not there.
+        print("Contact Verification failed...\nClearing Contacts for Saftey...")
+        myFile = open("contacts.json","r+")
+        myFile.truncate(0)
+        myFile.write("{\n\t\"contacts\": [] \n}")
+        myFile.seek(0)
+        encryptContacts(key)
+        exit()
+   
+    #if the file was not tampered with, decrypt and store data normally.
+    myFile = open("contacts.json","r+")
+    myFile.truncate(0)
+    myFile.write(data.decode())
